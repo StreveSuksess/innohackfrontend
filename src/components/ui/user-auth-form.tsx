4cf7a0e4fb3547e24529feab48d11be1a3892e91
CodeHun7er <codehun7er@gmail.com>
2024-09-28 12:39:14 +0300

    fixed something

--- src/components/ui/user-auth-form.tsx
+++ src/components/ui/user-auth-form.tsx
@@ -1,68 +1,37 @@
-import { Button } from "@/components/ui/button.tsx";
-import { Icons } from "@/components/ui/icons";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { cn } from "@/lib/utils";
-import { HTMLAttributes } from "react";
-import { useForm } from "react-hook-form";
-
+import { Button } from '@/components/ui/button.tsx'
+import { Icons } from '@/components/ui/icons'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { cn } from '@/lib/utils'
+import { HTMLAttributes } from 'react'
+import { useForm } from 'react-hook-form'
 
 interface UserAuthFormProps extends HTMLAttributes<HTMLDivElement> {
-  login?: boolean;
-  onSubmit: (data: any) => void;
-  isLoading: boolean;
+	login?: boolean
+	onSubmit: (data: any) => void
+	isLoading: boolean
 }
 
 export function UserAuthForm({
-  className,
-  login,
-  onSubmit,
-  isLoading,
-  ...props
+	className,
+	login,
+	onSubmit,
+	isLoading,
+	...props
 }: UserAuthFormProps) {
-  const { register, handleSubmit } = useForm();
+	const { register, handleSubmit } = useForm()
 
-  return (
-    <div className={cn("grid gap-6", className)} {...props}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="grid gap-2">
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="email">
-              Email
-            </Label>
+	return (
+		<div className={cn('grid gap-6', className)} {...props}>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<div className='grid gap-2'>
+					<div className='grid gap-1'>
+						<Label className='sr-only' htmlFor='email'>
+							Email
+						</Label>
 
-            <Input
-              {...register("email")}
-              id="email"
-              placeholder="name@example.com"
-              type="email"
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
-              disabled={isLoading}
-            />
-
-            {login && (
-              <>
-                <Label className="sr-only" htmlFor="password">
-                  Password
-                </Label>
-                <Input
-                  {...register("password")}
-                  id="password"
-                  placeholder="password"
-                  type="password"
-                  autoCapitalize="none"
-                  autoComplete="email"
-                  autoCorrect="off"
-                  disabled={isLoading}
-                />
-              </>
-            )}
-          </div>
-
-<<<<<<< HEAD
 						<Input
+							{...register('email')}
 							id='email'
 							placeholder='name@example.com'
 							type='email'
@@ -78,6 +47,7 @@ export function UserAuthForm({
 									Password
 								</Label>
 								<Input
+									{...register('password')}
 									id='password'
 									placeholder='password'
 									type='password'
@@ -90,6 +60,33 @@ export function UserAuthForm({
 						)}
 					</div>
 
+					<Input
+						id='email'
+						placeholder='name@example.com'
+						type='email'
+						autoCapitalize='none'
+						autoComplete='email'
+						autoCorrect='off'
+						disabled={isLoading}
+					/>
+
+					{login && (
+						<>
+							<Label className='sr-only' htmlFor='password'>
+								Password
+							</Label>
+							<Input
+								id='password'
+								placeholder='password'
+								type='password'
+								autoCapitalize='none'
+								autoComplete='email'
+								autoCorrect='off'
+								disabled={isLoading}
+							/>
+						</>
+					)}
+
 					<Button variant={'default'} disabled={isLoading}>
 						{isLoading && (
 							<Icons.spinner className='mr-2 h-4 w-4 animate-spin' />
@@ -100,16 +97,4 @@ export function UserAuthForm({
 			</form>
 		</div>
 	)
-=======
-          <Button variant={"default"} disabled={isLoading}>
-            {isLoading && (
-              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-            )}
-            {login ? "Log in" : "Sign Up"}
-          </Button>
-        </div>
-      </form>
-    </div>
-  );
->>>>>>> 9c9a1bcb3e1e3738bdce16da22ddbf2feda58e43
 }
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,12 +1,27 @@
-<<<<<<< HEAD
 import { buttonVariants } from '@/components/ui/button'
 import { UserAuthForm } from '@/components/ui/user-auth-form'
+import { toast } from '@/hooks/use-toast.ts'
 import { cn } from '@/lib/utils'
+import { useRegistrationMutation } from '@/services/authApi.ts'
 import { Link } from 'react-router-dom'
 
 export const SignUpPage = () => {
+	const [registration, { isLoading }] = useRegistrationMutation()
+	const onSubmit = async (data: { email: string }) => {
+		try {
+			await registration(data.email)
+			toast({
+				title: 'Check your email!',
+				description: 'Find your password on your email',
+			})
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
 	return (
 		<>
+			<div className='md:hidden'></div>
 			<div className='container relative hidden h-[100vh] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0'>
 				<Link
 					to='/login'
@@ -30,107 +45,10 @@ export const SignUpPage = () => {
 								Enter your email below to create your account
 							</p>
 						</div>
-						<UserAuthForm />
+						<UserAuthForm onSubmit={onSubmit} isLoading={isLoading} />
 					</div>
 				</div>
 			</div>
 		</>
 	)
 }
-=======
-import { buttonVariants } from "@/components/ui/button";
-import { UserAuthForm } from "@/components/ui/user-auth-form";
-import { toast } from "@/hooks/use-toast.ts";
-import { cn } from "@/lib/utils";
-import { useRegistrationMutation } from "@/services/authApi.ts";
-import { Link } from "react-router-dom";
-
-
-export const SignUpPage = () => {
-  const [registration, { isLoading }] = useRegistrationMutation();
-  const onSubmit = async (data: { email: string }) => {
-    try {
-      await registration(data.email);
-      toast({
-        title: "Check your email!",
-        description: "Find your password on your email",
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <>
-      <div className="md:hidden">
-        <img
-          src="/examples/authentication-light.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="block dark:hidden"
-        />
-        <img
-          src="/examples/authentication-dark.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="container relative hidden h-[100vh] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Link
-          to="/login"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
-        >
-          Login
-        </Link>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Acme Inc
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
-              </p>
-              <footer className="text-sm">Sofia Davis</footer>
-            </blockquote>
-          </div>
-        </div>
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
-              </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
-              </p>
-            </div>
-            <UserAuthForm onSubmit={onSubmit} isLoading={isLoading} />
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
->>>>>>> 9c9a1bcb3e1e3738bdce16da22ddbf2feda58e43
